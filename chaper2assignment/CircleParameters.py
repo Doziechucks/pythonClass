fad55b040d4f56157fa5de1af0ecd6c22a642c8b
Doziechucks <chiedoziechukwukam@gmail.com>
2024-10-21 09:10:51 +0100

    monthly pay

--- chaper2assignment/CircleParameters.py
+++
@@ -1,5 +0,0 @@
-radius = 2
-diameter = 2 * radius
-Circumference = 2 * 3.142 * radius
-Area = 3.14159 * (radius)**2
-print('diameter is ', diameter, 'circumference is ', Circumference, 'Area is', Area)
--- chaper2assignment/areaCircle.py
+++
@@ -1,3 +0,0 @@
-radius = float(input("Enter radius of the circle: "))
-areaOfcircle = 3.142 * (radius)**2
-print("Area of the circle is ", areaOfcircle)
