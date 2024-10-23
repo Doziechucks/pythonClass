e8b244715d1201dfcbe8c51680310fef4e3c24ed
Doziechucks <chiedoziechukwukam@gmail.com>
2024-10-23 21:08:10 +0100

    bmi calculator

---
+++ BMICalc.py
@@ -0,0 +1,7 @@
+weightInKilograms = int(input("Enter weight in kilograms: "))
+
+heightInMeters = int(input("Enter height in meters: "))
+
+BMI = weightInKilograms / (heightInMeters * heightInMeters)
+
+print(f"your body mass index is {BMI}")
