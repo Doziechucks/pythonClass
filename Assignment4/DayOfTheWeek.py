2b84c23e40d7abe96b5b4c01ce3b8b4b4a3ac69f
Doziechucks <chiedoziechukwukam@gmail.com>
2024-10-30 19:34:39 +0100

    printing day of the week from number

--- Assignment4/DayOfTheWeek.py
+++ Assignment4/DayOfTheWeek.py
@@ -1,1 +1,15 @@
-numberInput = int(input)
+numberInput = int(input("enter day number: "))
+if numberInput == 1:
+   print("Monday")
+if numberInput == 2:
+   print("Tuesday")
+if numberInput == 3:
+   print("Wednesday")
+if numberInput == 4:
+   print("Thursday")
+if numberInput == 5:
+   print("Friday")
+if numberInput == 6:
+   print("Saturday")
+if numberInput == 7:
+   print("Sunday")
