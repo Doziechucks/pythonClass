fe129e4e3053bad8964b7473c38b9b7bc9afa5d7
Doziechucks <chiedoziechukwukam@gmail.com>
2024-12-02 14:50:39 +0100

    adding random numbers

--- phaseGateOne/randomadd.py
+++ phaseGateOne/randomadd.py
@@ -1,8 +1,8 @@
 from random import randint
-variableOne = randit(1, 100)
-variableTwo = randit(1, 100)
+variableOne = randint(1, 100)
+variableTwo = randint(1, 100)
 summation = variableOne + variableTwo
-userInput = int(print(f"{variableOne} + {variableTwo}"))
+userInput = int(input(f"{variableOne} + {variableTwo}: "))
 if userInput == summation:
    print(True)
 else:
