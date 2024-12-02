acb75169d9c4737ea1c8d80b597efc0cd30f804b
Doziechucks <chiedoziechukwukam@gmail.com>
2024-12-02 14:45:59 +0100

    add digit

---
+++ phaseGateOne/digitadd.py
@@ -0,0 +1,6 @@
+userInput = input("Enter a number between 1 to 1000: ")
+summation = 0
+for number in userInput:
+   summation += int(number)
+  
+print(summation)
---
+++ phaseGateOne/randomadd.py
@@ -0,0 +1,9 @@
+from random import randint
+variableOne = randit(1, 100)
+variableTwo = randit(1, 100)
+summation = variableOne + variableTwo
+userInput = int(print(f"{variableOne} + {variableTwo}"))
+if userInput == summation:
+   print(True)
+else:
+   print(False)
